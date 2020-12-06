20cc4cd5f6c1c679eea99195b51101ce0f6f4665
Amaury Carrade <amaury@carrade.eu>
2020-12-06 15:32:01 +0100

    Day 6 — Custom Customs — Improved
    Groups are calculated once and calculation is way simpler

--- src/main/scala/adventofcode/days/Day06.scala
+++ src/main/scala/adventofcode/days/Day06.scala
@@ -3,16 +3,12 @@ package adventofcode.days
 import adventofcode.Day
 
 object Day06 extends Day(6) {
-  private def parse(lines: Seq[String]) = (lines :+ "").foldLeft((Seq[String](), Seq[Seq[String]]())) {
-    case ((current, all), line) => line match {
-      case "" => (Seq[String](), all :+ current)
-      case _ => (current :+ line, all)
-    }
-  }._2
 
-  override def firstSolution = parse(lines).map(_.flatten.toSet.size).sum
+  val groups = input.split("\n\n").map(_.split("\n"))
 
-  override def secondSolution = parse(lines).map(_.foldLeft(null: Set[Char]) {
+  override def firstSolution = groups.map(_.flatten.distinct.length).sum
+
+  override def secondSolution = groups.map(_.foldLeft(null: Set[Char]) {
     case (intersect, answer) => intersect match {
       case null => answer.toSet
       case _ => intersect.intersect(answer.toSet)
